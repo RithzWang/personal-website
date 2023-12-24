356e3eda6400ff4d3c2dc1218b24b886ed643888
Rithz <101114513+RithzWang@users.noreply.github.com>
2023-12-24 09:26:37 +0700

    Update

---
+++ script.js
@@ -0,0 +1,31 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const snowfallContainer = document.getElementById("snowfall");
+
+    function createSnowflake() {
+        const snowflake = document.createElement("div");
+        snowflake.className = "snowflake";
+        snowfallContainer.appendChild(snowflake);
+
+        const size = Math.random() * 3 + 2;
+        snowflake.style.width = `${size}px`;
+        snowflake.style.height = `${size}px`;
+
+        const startPosition = Math.random() * window.innerWidth;
+        snowflake.style.left = `${startPosition}px`;
+
+        const animationDuration = Math.random() * 3 + 2;
+        snowflake.style.animation = `fall ${animationDuration}s linear infinite`;
+
+        setTimeout(() => {
+            snowflake.remove();
+        }, animationDuration * 1000);
+    }
+
+    function generateSnowflakes() {
+        setInterval(createSnowflake, 100); // Decreased interval for more snowflakes
+    }
+
+    generateSnowflakes();
+});
