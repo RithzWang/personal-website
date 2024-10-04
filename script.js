b7922f7776ac94889a8806fd0eda85408d0c79a5
Rithz <101114513+RithzWang@users.noreply.github.com>
2024-10-04 11:18:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-// script.js
+const toggleButton = document.getElementById('toggleButton');
+const toggleImage = document.getElementById('toggleImage');
 
-document.addEventListener("DOMContentLoaded", function () {
-    const snowfallContainer = document.getElementById("snowfall");
-
-    function createSnowflake() {
-        const snowflake = document.createElement("div");
-        snowflake.className = "snowflake";
-        snowfallContainer.appendChild(snowflake);
-
-        const size = Math.random() * 3 + 2;
-        snowflake.style.width = `${size}px`;
-        snowflake.style.height = `${size}px`;
-
-        const startPosition = Math.random() * window.innerWidth;
-        snowflake.style.left = `${startPosition}px`;
-
-        const animationDuration = Math.random() * 3 + 2;
-        snowflake.style.animation = `fall ${animationDuration}s linear infinite`;
-
-        setTimeout(() => {
-            snowflake.remove();
-        }, animationDuration * 1000);
-    }
-
-    function generateSnowflakes() {
-        setInterval(createSnowflake, 50); // Decreased interval for more snowflakes
-    }
-
-    generateSnowflakes();
+toggleButton.addEventListener('click', function() {
+  if (toggleImage.style.display === 'none') {
+    toggleImage.style.display = 'block';
+    toggleButton.textContent = 'Hide Image';
+  } else {
+    toggleImage.style.display = 'none';
+    toggleButton.textContent = 'Show Image';
+  }
+});
